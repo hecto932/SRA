d3881434c8f2b83b03328fcd5eff802f609a0a6e
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2015-04-02 01:38:04 -0430

    Maquetacion

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'hflores';
+$db['default']['password'] = '20162504';
+$db['default']['database'] = 'sra';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
